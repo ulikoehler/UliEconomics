3118358634f61c82400854feaa4d49b99850532f
Uli Köhler <ukoehler@techoverflow.net>
2024-06-16 18:57:52 +0200

    Fix syntax

--- UliEconomics/VariableCostFunctions/LinearTimeVariableCostFunction.py
+++ UliEconomics/VariableCostFunctions/LinearTimeVariableCostFunction.py
@@ -14,7 +14,7 @@ class LinearTimeVariableCostFunction(TimeVariableCostFunction):
     def __init__(self, t0: pd.Timestamp, increase_factor: float, per=pd.Timedelta):
         self.t0 = t0
         # Compute additive cost per second since t0
-        self.factor_per_second = self.increase / per.total_seconds()
+        self.factor_per_second = increase_factor / per.total_seconds()
         # Save original inputs for __str__() etc
         self.increase_factor = increase_factor
         self.per = per
@@ -28,6 +28,11 @@ class LinearTimeVariableCostFunction(TimeVariableCostFunction):
         If the function return >1.0, the resulting cost will be increased
         """
         time_since_t0 = (timestamp - self.t0).total_seconds
-        return 1.0 + self.factor_per_second()
+        return 1.0 + self.factor_per_second * time_since_t0
 
-    def 
+    def __str__(self):
+        return f"LinearTimeVariableCostFunction(t0={self.t0}, increase_factor={self.increase_factor}, per={self.per})"
+    
+    def __repr__(self):
+        return str(self)
+    
